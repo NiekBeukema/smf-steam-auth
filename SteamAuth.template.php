d07d6676a2bbe09a56b5f41d4696b64a51a4ad20
Niek Beukema <15339146+NiekBeukema@users.noreply.github.com>
2019-09-18 15:24:10 +0200

    Update SteamAuth.template.php

--- SteamAuth.template.php
+++ SteamAuth.template.php
@@ -10,7 +10,7 @@ function template_steam_login_below()
 	echo '
 	<center>
 		<form action="', $scripturl, '?action=login;steam" method="post">
-			<input type="image" src="http://cdn.steamcommunity.com/public/images/signinthroughsteam/sits_large_border.png">
+			<input type="image" src="https://cdn.steamcommunity.com/public/images/signinthroughsteam/sits_large_border.png">
 		</form>
 	</center>';
 }
