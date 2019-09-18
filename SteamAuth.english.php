6c3635507a511346f54c5e8c5e263942199afb2b
Niek Beukema <15339146+NiekBeukema@users.noreply.github.com>
2019-09-18 15:22:26 +0200

    Update SteamAuth.english.php

--- SteamAuth.english.php
+++ SteamAuth.english.php
@@ -5,7 +5,7 @@
 // Important! Before editing these language files please read the text at the top of index.english.php.
 
 $txt['steam_auth_api_key'] = 'Steam API key';
-$txt['steam_auth_api_key_link'] = '<a href="http://steamcommunity.com/dev/apikey" target="_blank">Get one</a>';
+$txt['steam_auth_api_key_link'] = '<a href="https://steamcommunity.com/dev/apikey" target="_blank">Get one</a>';
 
 $txt['steam'] = 'Steam';
 $txt['steam_auth_x'] = 'User has cancelled authentication';
